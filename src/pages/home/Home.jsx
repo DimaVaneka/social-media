617feab0d49e5e5f7551b3137f0a3bcac5ab6a4b
unknown <dimavaneka@gmail.com>
2023-04-05 00:24:35 +0300

    init commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import "./home.scss"
+import HomeIcon from '@mui/icons-material/Home';
+
+
+export const Home = () => {
+  return (
+    <div className='home'>
+        <h1>home</h1>
+        <h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1><h1>home</h1>
+    </div>
+  )
+}
+
+export default Home
+
